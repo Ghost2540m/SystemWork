5dace7a6278848c76568fe5b9dfa4438aea02a72
Ghost2540m <94738548+Ghost2540m@users.noreply.github.com>
2022-02-19 19:08:01 +0330

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace SystemWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("B Nazanin", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(70, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Lock Station";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("B Nazanin", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.button2.Location = new System.Drawing.Point(70, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(300, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Empty Recycle Bin";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("B Nazanin", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.Location = new System.Drawing.Point(70, 175);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(300, 30);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Calculate Time";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("B Nazanin", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button4.ForeColor = System.Drawing.Color.Yellow;
+            this.button4.Location = new System.Drawing.Point(70, 211);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(300, 30);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Restart Program";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("B Nazanin", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(377, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 38);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "کار با سیستم";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(533, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(301, 281);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Aquamarine;
+            this.label2.Location = new System.Drawing.Point(754, 326);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "سازنده : Ghost ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 326);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "stragesaim@gmail.com";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(842, 348);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "مدیریت سیستم";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace SystemWork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //قفل کردن کامپیوتر   
+        [DllImport("user32.dll")]
+        public static extern void LockWorkStation();
+        //خالی کردن سطل  ویندوز
+        enum RecycleFlags : uint
+        {
+            SHERB_NOCONFIRMATION = 0x00000001,
+            SHERB_NOPROGRESSUI = 0x00000001,
+            SHERB_NOSOUND = 0x00000004
+        }
+        [DllImport("Shell32.dll", CharSet = CharSet.Unicode)]
+        static extern uint SHEmptyRecycleBin(IntPtr hwnd, string pszRootPath,
+        RecycleFlags dwFlags);
+         [DllImport("user32")]
+        internal static extern IntPtr GetDC(IntPtr hwnd);
+        [DllImport("User32.dll")]
+        internal static extern void ReleaseDC(IntPtr dc);
+        public void PaintRectangleToScreen()
+        {
+                IntPtr deskDC = GetDC(IntPtr.Zero);
+                Graphics g = Graphics.FromHdc(deskDC);
+                Font font = new Font("Arial Black", 36);                
+                DrawStringOnCenter(g, "Desktop Writing!", font, new Point(0, -25));
+                font = new Font("Arial Black", 18);
+                DrawStringOnCenter(g, "by: Ramzan", font, new Point(0, 25));
+                Rectangle rect = new Rectangle(200, 300, 
+                    Screen.PrimaryScreen.Bounds.Width - 400, 
+                    Screen.PrimaryScreen.Bounds.Height - 600);
+                g.FillRectangle(new SolidBrush(Color.FromArgb(100, Color.DodgerBlue))
+                    , rect);
+                g.DrawRectangle(new Pen(Color.DodgerBlue, 3), rect);
+                g.Dispose();
+                ReleaseDC(deskDC);
+            }
+        private void DrawStringOnCenter(Graphics g, string str, Font font, Point offset)
+        {
+            SizeF size = g.MeasureString(str, font);
+            g.DrawString(str, font, Brushes.White,
+                      new PointF(
+                                  (Screen.PrimaryScreen.Bounds.Width - size.Width) / 2 + offset.X,
+                                  ( Screen.PrimaryScreen.Bounds.Height - size.Height) / 2 + offset.Y)
+                                 );
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LockWorkStation();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            uint result = SHEmptyRecycleBin(IntPtr.Zero, null, 0);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //محاسبه زمان اجرای قسمتی برنامه 
+            // Create an instance of the new StopWatch class
+            Stopwatch myWatch = new Stopwatch();
+            // Start the timer
+            myWatch.Start();
+            //Code Here...
+            // Now we can stop the timer and display the elapsed time along
+            for (int i = 1; i <= 10000; i++)
+                for (int j = 1; j <= 100000; j++) ; 
+
+            myWatch.Stop();
+            MessageBox.Show( myWatch.ElapsedMilliseconds.ToString());
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // ریست کردن برنامه کردن برنامه
+            Application.Restart();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+               PaintRectangleToScreen();
+         }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+             
+            label1.BackColor = Color.Transparent;
+           
+            label1.BackColor = Color.FromArgb(127, label1.BackColor);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("ILIYA ON TOP ");
+        }
+    }
+}
